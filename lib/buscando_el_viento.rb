1a3942bfb8c12119fabe5614d3cc32d21b9729e0
Giles Bowkett <gilesb@gmail.com>
2011-07-11 23:25:25 -0700

    refactored to modules

--- lib/buscando_el_viento.rb
+++ lib/buscando_el_viento.rb
@@ -2,24 +2,26 @@ require 'active_record'
 require 'active_record/base' # https://github.com/rails/rails/pull/1999
 
 module BuscandoElViento
-  class Migration < ActiveRecord::Migration
 
-    def self.vector_name(column)
+  module Vectors
+    def vector_name(column)
       "#{column}_search_vector".to_sym
     end
-    def self.add_search_vector(table, column)
+    def add_search_vector(table, column)
       add_column(table, vector_name(column), "tsvector")
     end
-    def self.remove_search_vector(table, column)
+    def remove_search_vector(table, column)
       remove_column(table, vector_name(column))
     end
+  end
 
+  module Triggers
     # FIXME: it may be worthwhile to separate these concerns, and their specs, into
     # distinct sub-modules
-    def self.trigger_name(table, column)
+    def trigger_name(table, column)
       "#{table}_#{column}_search_vector_update"
     end
-    def self.add_trigger(table, column)
+    def add_trigger(table, column)
       execute <<TRIGGER
 CREATE TRIGGER #{trigger_name(table, column)}
 BEFORE INSERT OR UPDATE
@@ -30,10 +32,14 @@ tsvector_update_trigger(#{vector_name(column).to_s},
                         #{column});
 TRIGGER
     end
-    def self.remove_trigger(table, column)
+    def remove_trigger(table, column)
       execute "DROP TRIGGER IF EXISTS #{trigger_name(table, column)} on #{table};"
     end
 
+  end
+  class Migration < ActiveRecord::Migration
+    extend Vectors
+    extend Triggers
   end
 end
 
