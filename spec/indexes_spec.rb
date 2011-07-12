2c23fec56770a1956a7fac729f24beab15184587
Giles Bowkett <gilesb@gmail.com>
2011-07-12 13:43:50 -0700

    removed some pending specs which were just dumb

--- spec/indexes_spec.rb
+++ spec/indexes_spec.rb
@@ -21,13 +21,11 @@ CREATE_INDEX
     SearchMigration.should_receive(:execute).with(create_index)
     SearchMigration.add_index(:users, :username)
   end
-  it "removes indexes"
 
   it "names composite indexes for searching multiple attributes"
   it "creates composite indexes for searching multiple attributes"
   it "removes composite indexes for searching multiple attributes"
 
-  it "names multiple indexes for searching distinct attributes"
   it "creates multiple indexes for searching distinct attributes" do
     create_username_index = <<-CREATE_INDEX
 CREATE INDEX index_users_on_username_search_vector
@@ -45,6 +43,5 @@ CREATE_INDEX
     SearchMigration.should_receive(:execute).with(create_email_index)
     SearchMigration.add_index(:users, :email)
   end
-  it "removes multiple indexes for searching distinct attributes"
 end
 
