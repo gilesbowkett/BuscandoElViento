7a5f40a646a9485c105128c9b2225d60843529c5
Giles Bowkett <gilesb@gmail.com>
2011-07-11 22:31:01 -0700

    add_search_vector method exists

--- lib/buscando_el_viento.rb
+++ lib/buscando_el_viento.rb
@@ -3,6 +3,9 @@ require 'active_record/base' # https://github.com/rails/rails/pull/1999
 
 module BuscandoElViento
   class Migration < ActiveRecord::Migration
+    def self.add_search_vector(table, column, fuzzy)
+      add_column
+    end
   end
 end
 
--- spec/buscando_el_viento_spec.rb
+++ spec/buscando_el_viento_spec.rb
@@ -8,4 +8,11 @@ describe BuscandoElViento do
   it "inherits" do
     BuscandoElViento::Migration.new.should be_a(ActiveRecord::Migration)
   end
+
+  it "auto-adds search vectors" do
+    class SearchMigration < BuscandoElViento::Migration; end
+
+    SearchMigration.should_receive(:add_column)
+    SearchMigration.add_search_vector :users, :username, :fuzzy => true
+  end
 end
