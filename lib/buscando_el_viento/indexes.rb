ee1dcba44d297d02581262cfc00e6977cbf015ae
Giles Bowkett <gilesb@gmail.com>
2011-07-12 13:42:44 -0700

    add_index

--- lib/buscando_el_viento/indexes.rb
+++ lib/buscando_el_viento/indexes.rb
@@ -2,4 +2,14 @@ module BuscandoElViento
   def index_name(table, column) # FIXME: better variable names
     "index_#{table}_on_#{column}_search_vector"
   end
+
+  # yes, we're overwriting ActiveRecord::Migration's add_index, which, in this
+  # context, is less useful than it should be
+  def add_index(table, column)
+    execute <<-ADD_INDEX
+CREATE INDEX #{index_name(table, column)}
+ON #{table}
+USING gin(search_vector);
+ADD_INDEX
+  end
 end
--- spec/indexes_spec.rb
+++ spec/indexes_spec.rb
@@ -1,6 +1,8 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe BuscandoElViento do
+  # FIXME: these specs are sufficiently similar that I should probably be either subclassing
+  #        specs, or bunging this before(:each) in the helper
   before(:each) do
     class SearchMigration < BuscandoMigration
     end
@@ -10,7 +12,15 @@ describe BuscandoElViento do
     index_name = "index_users_on_username_search_vector"
     SearchMigration.index_name(:users, :username).should eq(index_name)
   end
-  it "creates indexes"
+  it "creates indexes" do
+    create_index = <<-CREATE_INDEX
+CREATE INDEX index_users_on_username_search_vector
+ON users
+USING gin(search_vector);
+CREATE_INDEX
+    SearchMigration.should_receive(:execute).with(create_index)
+    SearchMigration.add_index(:users, :username)
+  end
   it "removes indexes"
 
   it "names composite indexes for searching multiple attributes"
@@ -18,7 +28,23 @@ describe BuscandoElViento do
   it "removes composite indexes for searching multiple attributes"
 
   it "names multiple indexes for searching distinct attributes"
-  it "creates multiple indexes for searching distinct attributes"
+  it "creates multiple indexes for searching distinct attributes" do
+    create_username_index = <<-CREATE_INDEX
+CREATE INDEX index_users_on_username_search_vector
+ON users
+USING gin(search_vector);
+CREATE_INDEX
+    SearchMigration.should_receive(:execute).with(create_username_index)
+    SearchMigration.add_index(:users, :username)
+
+    create_email_index = <<-CREATE_INDEX
+CREATE INDEX index_users_on_email_search_vector
+ON users
+USING gin(search_vector);
+CREATE_INDEX
+    SearchMigration.should_receive(:execute).with(create_email_index)
+    SearchMigration.add_index(:users, :email)
+  end
   it "removes multiple indexes for searching distinct attributes"
 end
 
