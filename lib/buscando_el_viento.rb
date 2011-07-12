7653ae33ce780a1c0ca1dfd2bff961d7cbe0e9b9
Giles Bowkett <gilesb@gmail.com>
2011-07-11 22:37:08 -0700

    remove_search_vector, tiny refactor

--- lib/buscando_el_viento.rb
+++ lib/buscando_el_viento.rb
@@ -3,9 +3,14 @@ require 'active_record/base' # https://github.com/rails/rails/pull/1999
 
 module BuscandoElViento
   class Migration < ActiveRecord::Migration
+
     def self.add_search_vector(table, column)
       add_column(table, "#{column}_search_vector".to_sym, "tsvector")
     end
+    def self.remove_search_vector(table, column)
+      remove_column(table, "#{column}_search_vector".to_sym)
+    end
+
   end
 end
 
--- spec/buscando_el_viento_spec.rb
+++ spec/buscando_el_viento_spec.rb
@@ -1,6 +1,11 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe BuscandoElViento do
+  before(:each) do
+    class SearchMigration < BuscandoElViento::Migration
+    end
+  end
+
   it "exists" do
     BuscandoElViento.class.should == Module
   end
@@ -10,11 +15,15 @@ describe BuscandoElViento do
   end
 
   it "auto-adds search vectors" do
-    class SearchMigration < BuscandoElViento::Migration; end
-
     SearchMigration.should_receive(:add_column).with(:users,
                                                      :username_search_vector,
                                                      "tsvector")
     SearchMigration.add_search_vector :users, :username
+  end
+
+  it "removes search vectors" do
+    SearchMigration.should_receive(:remove_column).with(:users,
+                                                        :username_search_vector)
+    SearchMigration.remove_search_vector :users, :username
   end
 end
